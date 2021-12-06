eda043804b9a4ee022a497e55126ba93c2ef617e
James Barry <jbm03003@gmail.com>
2021-12-06 18:30:35 -0500

    reorganized code and fixed Prompt buttons to confirm

--- Develop/script.js
+++ Develop/script.js
@@ -1,51 +1,5 @@
-// Variables for prompts
-var confirm;
-var lowerCasePrompt;
-var upperCasePrompt;
-var symbolsPrompt;
-var numbersPrompt;
-
-// Variables for characters
-var lowerCase = "abcdefghijklmnopqrstuvwxyz";
-var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";  
-var numbers = "0123456789";
-var symbols = "!@#$%^&*()_+{}[]>?,./";
-
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-
-// Start password function
-function generatePassword() {
-  var password = "";
-  var passwordChar = "";
-  confirm = prompt("Please choose password length between 8-128 characters");
-
-  // if statement in which the characters prompt isn't fulfilled
-  if (confirm < 8 || confirm > 128) {
-    confirm = alert("Must be between 8-128 characters!");
-    return "";
-  
-  // else/if for each different symbol  
-  } else {
-    lowerCasePrompt = prompt("Would you like lower case characters in your password?");
-    upperCasePrompt = prompt("Would you like upper case characters in your password?");
-    symbolsPrompt = prompt("Would you like symbols in your password?");
-    numbersPrompt = prompt ("Would you like numbers in your password?");    
-  
-  } if (lowerCasePrompt) {
-    passwordChar += lowerCase;
-  
-  } if (upperCasePrompt) {
-    passwordChar += upperCase;
-  
-  } if (symbolsPrompt) {
-    passwordChar += symbols;
-  
-  } if (numbersPrompt) {
-    passwordChar += numbers;
-  }
-};
 
 // Write password to the #password input
 function writePassword() {
@@ -54,7 +8,59 @@ function writePassword() {
 
   passwordText.value = password;
 
-}
+};
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+// Start password function
+function generatePassword() {
+  // Variables for characters
+  var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";  
+  var numbers = "0123456789";
+  var symbols = "!@#$%^&*()_+{}[]>?,./";
+
+  var characterTypes = "";
+  var finalPassword = "";
+  
+ passwordChar();
+
+ function passwordChar() {
+  characterTypes = "";
+  
+  passwordLength = parseInt(prompt("Please choose password length between 8-128 characters"));
+
+  // if statement in which the characters prompt isn't fulfilled
+  if (passwordLength < 8 || passwordLength > 128) {
+    passwordLength = window.alert("Must be between 8-128 characters!");
+    return;
+  } 
+  
+    lowerCasePrompt = confirm("Would you like lower case characters in your password?");
+    upperCasePrompt = confirm("Would you like upper case characters in your password?");
+    symbolsPrompt = confirm("Would you like symbols in your password?");
+    numbersPrompt = confirm("Would you like numbers in your password?");
+  }      
+  
+  if (lowerCasePrompt === true) {
+    characterTypes += lowerCase;
+
+  } if (upperCasePrompt === true) {
+    characterTypes += upperCase;
+  
+  } if (symbolsPrompt === true) {
+    characterTypes += symbols;
+  
+  } if (numbersPrompt === true) {
+    characterTypes += numbers;  
+  } 
+
+
+// For loop for password length
+  for (var i = passwordLength; i < 0; i--) {
+    finalPassword = passwordChar[Math.floor(Math.random() * passwordLength.length)];
+  } 
+  return finalPassword;
+};
