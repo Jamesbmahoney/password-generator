67e084d41a760c710e771df975337c4bf5f6c8ef
James Barry <jbm03003@gmail.com>
2021-12-06 18:54:37 -0500

    added pseudo code for accessibility

--- Develop/script.js
+++ Develop/script.js
@@ -13,21 +13,24 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+// Character type variables
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";  
+var numbers = "0123456789";
+var symbols = "!@#$%^&*()_+{}[]>?,./";
+
+
+// Prompt variables
+var lowerCasePrompt = "";
+var upperCasePrompt = "";
+var symbolsPrompt = "";
+var numbersPrompt = "";
 
 // Start password function
 function generatePassword() {
-  // Variables for characters
-  var lowerCase = "abcdefghijklmnopqrstuvwxyz";
-  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";  
-  var numbers = "0123456789";
-  var symbols = "!@#$%^&*()_+{}[]>?,./";
-
+  // Variables for characters to return
   var characterTypes = "";
-  var finalPassword = "";
-  var lowerCasePrompt = "";
-  var upperCasePrompt = "";
-  var symbolsPrompt = "";
-  var numbersPrompt = "";
+  var finalPassword = "";  
   var finalCharacterTypes = "";
   var charactersTotal = "";
   
@@ -35,20 +38,20 @@ function generatePassword() {
 
 function passwordChar() {
   characterTypes = "";
-  
+  // Choice of password length
   passwordLength = parseInt(prompt("Please choose password length between 8-128 characters"))
-  // if statement in which the characters prompt isn't fulfilled
+  // If statement in which the characters prompt isn't fulfilled
   if (passwordLength < 8 || passwordLength > 128) {
     passwordLength = window.alert("Must be between 8-128 characters!");
     return;
   } 
-  
+    // Different prompts to add extra characters
     lowerCasePrompt = confirm("Would you like lower case characters in your password?");
     upperCasePrompt = confirm("Would you like upper case characters in your password?");
     symbolsPrompt = confirm("Would you like symbols in your password?");
     numbersPrompt = confirm("Would you like numbers in your password?");
   }      
-  
+  // If statements to confirm use of extra characters 
   if (lowerCasePrompt === true) {
     characterTypes += lowerCase;
 
