9a664971fc5d2200eb80b0da246de2d59bceb66b
James Barry <jbm03003@gmail.com>
2021-12-06 15:39:33 -0500

    added array for random elements

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,28 @@
 // Assignment code here
+var randomArray = {
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNumber,
+  symbol: getRandomSymbol
+};
+
+
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function getRandomSymbol() {
+  var symbols = '!@#$%^&*(){}[]+<>?,./'
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
