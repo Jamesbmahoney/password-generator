bbe1c76057cb055fff4c8a1d99084d0a00ebfbdd
James Barry <jbm03003@gmail.com>
2021-12-06 16:58:26 -0500

    added prompts for each set of characters to choose

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,11 @@
-// Variables for characters to be generated
-var confirm
+// Variables for prompts
+var confirm;
+var lowerCasePrompt;
+var upperCasePrompt;
+var symbolsPrompt;
+var numbersPrompt;
+
+// Variables for characters
 var lowerCase = "abcdefghijklmnopqrstuvwxyz";
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";  
 var numbers = "0123456789";
@@ -9,15 +15,37 @@ var symbols = "!@#$%^&*()_+{}[]>?,./";
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+// Start password function
 function generatePassword() {
   var password = "";
   var passwordChar = "";
   confirm = prompt("Please choose password length between 8-128 characters");
 
+  // if statement in which the characters prompt isn't fulfilled
   if (confirm < 8 || confirm > 128) {
-    confirm = prompt("Must be between 8-128 characters!");
+    confirm = alert("Must be between 8-128 characters!");
+    return "";
+  
+  // else/if for each different symbol  
+  } else {
+    lowerCasePrompt = prompt("Would you like lower case characters in your password?");
+    upperCasePrompt = prompt("Would you like upper case characters in your password?");
+    symbolsPrompt = prompt("Would you like symbols in your password?");
+    numbersPrompt = prompt ("Would you like numbers in your password?");    
+  
+  } if (lowerCasePrompt) {
+    passwordChar += lowerCase;
+  
+  } if (upperCasePrompt) {
+    passwordChar += upperCase;
+  
+  } if (symbolsPrompt) {
+    passwordChar += symbols;
+  
+  } if (numbersPrompt) {
+    passwordChar += numbers;
   }
-}
+};
 
 // Write password to the #password input
 function writePassword() {
