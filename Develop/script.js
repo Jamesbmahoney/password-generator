9bd9e3cb51a030c5216a6749ae7d9705edbd5953
James Barry <jbm03003@gmail.com>
2021-12-06 16:19:50 -0500

    created a command for number of characters to use

--- Develop/script.js
+++ Develop/script.js
@@ -1,11 +1,23 @@
-// Assignment code here
-var lowerCase = "abcdefghijklmnopqrstuvwxyz"
-var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"  
-var numbers = "0123456789"
-var symbols = "!@#$%^&*()_+{}[]>?,./"
+// Variables for characters to be generated
+var confirm
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";  
+var numbers = "0123456789";
+var symbols = "!@#$%^&*()_+{}[]>?,./";
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+function generatePassword() {
+  var password = "";
+  var passwordChar = "";
+  confirm = prompt("Please choose password length between 8-128 characters");
+
+  if (confirm < 8 || confirm > 128) {
+    confirm = prompt("Must be between 8-128 characters!");
+  }
+}
 
 // Write password to the #password input
 function writePassword() {
